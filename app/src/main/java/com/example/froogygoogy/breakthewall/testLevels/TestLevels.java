69d05fd4e8d38f2d40e154248783b6c9267ca64d
Ghardian <al264066@uji.es>
2018-05-03 20:18:43 +0200

    2.5.2
    Acabado el 2.5.2

--- app/src/main/java/com/example/froogygoogy/breakthewall/testLevels/TestLevels.java
+++ app/src/main/java/com/example/froogygoogy/breakthewall/testLevels/TestLevels.java
@@ -2,12 +2,76 @@ package com.example.froogygoogy.breakthewall.testLevels;
 
 import android.util.DisplayMetrics;
 
+import com.example.froogygoogy.breakthewall.Assets;
 import com.example.froogygoogy.breakthewall.framework.GameActivity;
 import com.example.froogygoogy.breakthewall.framework.IGameController;
 import com.example.froogygoogy.breakthewall.testBricks.TestBrickController;
 
 public class TestLevels extends GameActivity {
     private TestBrickController controller;
+
+    public static final int NO_BRICK = -1;
+    private static final int BRICKS_IN_ROW = 10;
+    private static int[][][] colors;
+
+    static{
+
+        colors = new int[10][][];
+
+        //FIRST LEVEL
+        int nrows = 2;
+        colors[0] = new int[nrows][];
+
+        for (int row = 0;row < nrows; row++)
+        {
+            colors[0][row] = new int [BRICKS_IN_ROW];
+            for (int col = 0; col< BRICKS_IN_ROW; col ++)
+            {
+                colors[0][row][col] = (row + col) %% Assets.bricks.length;
+            }
+        }
+
+        //SECOND LEVEL
+        int nrows = 4;
+        colors[0] = new int[nrows][];
+
+        for (int row = 0;row < nrows; row++)
+        {
+            colors[0][row] = new int [BRICKS_IN_ROW];
+            for (int col = 0; col< BRICKS_IN_ROW; col ++)
+            {
+                colors[0][row][col] = (row + col) %% Assets.bricks.length;
+            }
+        }
+
+        //THIRD LEVEL
+        int nrows = 5;
+        colors[0] = new int[nrows][];
+
+        for (int row = 0;row < nrows; row++)
+        {
+            colors[0][row] = new int [BRICKS_IN_ROW];
+            for (int col = 0; col< BRICKS_IN_ROW; col ++)
+            {
+                colors[0][row][col] = (row + col) %% Assets.bricks.length;
+            }
+        }
+
+
+
+
+
+    }
+
+
+    public static int[][] getLevel(int n) {
+
+        if (n < colors.length) return colors[n];
+        else{
+            return colors[colors.length-1];
+        }
+    }
+    
 
     @Override
     protected IGameController buildGameController() {
@@ -16,4 +80,6 @@ public class TestLevels extends GameActivity {
         this.controller = new TestBrickController(displayMetrics.widthPixels,displayMetrics.heightPixels, this);
         return controller;
     }
+
+
 }
